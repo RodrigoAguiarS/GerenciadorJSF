f1c6b70011106cd8132571bb49616d90b2632221
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-01-09 21:07:30 -0300

    iniciando estudos em TesteUnitários com junit

---
+++ src/test/java/br/com/rodrigo/ExampleTest.java
@@ -0,0 +1,17 @@
+package br.com.rodrigo;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ExampleTest {
+    @Test
+    public void Teste() {
+
+        int a = 1 + 1;
+
+        assertEquals(2, a);
+    }
+}
